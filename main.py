6ecb57823c44321aa17a990e3d0a84d44e5804ee
Vlad Sevruk <vladworking5@gmail.com>
2024-12-12 14:26:28 +0300

    little change

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
-import json
+import json 
 
-with open('dump.json', 'r', encoding='utf-8') as file:
-    data = json.load(file)#чтение данных из файла в формате JSON и преобразование их в объекты Python
+kvalf = input("Введите номер квалификации: ")  
+find = False 
 
-#запрос номера квалификации у пользователя
-numb = input("Введите номер квалификации: ")
+with open("dump.json", 'r', encoding = 'utf-8') as file:
+    read_file = json.load(file)   
+    for skill in read_file: 
+        if skill.get("model") == "data.skill":
+            if skill["fields"].get("code") == kvalf:  
+                skill_code = skill["fields"].get("code") 
+                skill_title = skill["fields"].get("title")  
+                skill_specialty=skill["fields"].get("specialty")
+                find = True
 
-print("\n=============== Результат поиска ===============")
-for item in data:
-    if item['model'] == 'data.skill' and item['fields']['code'].startswith(numb):#startswith проверяет, начинается ли строка на указанную подстроку
-        print(f"{item['fields']['code']} >> Квалификация {item['fields']['title']}")
-        found = True
-    elif item['model'] == 'data.specialty' and item['fields']['code'].startswith(numb):
-        print(f"{item['fields']['code']} >> Специальность {item['fields']['title']}")
+if not find:  
+    print(" Не найдено ".center(60, "=")) 
+    exit() 
 
-if not found:
-    print("============== Не найдено ===============")
+for specialty in read_file:
+    if specialty.get("model") == "data.specialty": 
+        specialty_code = specialty["fields"].get("code")  
+        specialty_pk = specialty.get("pk") 
+
+
+        if skill_specialty == specialty_pk:
+            specialty_title = specialty["fields"].get("title") 
+            specialty_educational = specialty["fields"].get("c_type") 
+            specialty_c = specialty["fields"].get("code")
+
+
+else:
+    print("\n", " Найдено ".center(80, "="),'\n')
+    print(f"{specialty_c} >> Специальность '{specialty_title}', {specialty_educational}") 
+    print(f"{kvalf} >> Квалификация '{skill_title}'") 
